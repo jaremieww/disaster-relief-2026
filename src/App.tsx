e472cb8a7e207c3e8f4ba573c9c3b86249179e9b
jaremieww <156275759+jaremieww@users.noreply.github.com>
2026-01-18 23:13:57 -0500

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 import { Header } from './Header'
 import { Home } from './Home'
+import { About } from './About'
 import { Footer } from './Footer'
 import { CenterDetail } from './CenterDetail'
 import { PageNotFound } from './PageNotFound'
@@ -10,20 +11,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
       <Header />
       <Router>
         <Routes>
-          {/* <Route path="/" exact>
-            <Redirect to="/home" />
-          </Route> */}
-          <Route path="/" element={<Home />} exact />
-          <Route path="/home" element={<Home />} exact />
+          <Route path="/" element={<Home />}  />
+          <Route path="/home" element={<Home />}  />
+          <Route path="/about" element={<About />}  />
           <Route path="*" element={<Home />} />
-          // <Route path="/center" element={<CenterManager />} exact />
+          // <Route path="/center" element={<CenterManager />}  />
           // <Route path="/center/:id" element={<CenterDetail />} />
           // <Route path="/error" element={<PageNotFound />} />
         </Routes>
